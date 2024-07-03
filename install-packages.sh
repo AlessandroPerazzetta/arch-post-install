853f0824d2b1294042c4b5157b1ca07e54f05cc0
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2024-07-03 20:07:02 +0200

    Change group for serial permission to allow user read ttyUSB0

--- install-packages.sh
+++ install-packages.sh
@@ -100,17 +100,17 @@ then
                 ;;
             sys_serial)
                 printf "${YELLOW}Installing system permissions to allow user open Serial...\n${NC}"
-                grep -Ei "^dialout" /etc/group;
+                grep -Ei "^uucp" /etc/group;
                 if [ $? -eq 0 ]; then
-                    printf "${YELLOW}Dialout Group Exists add current user...\n${NC}"
-                    if id -nG "$CURRENT_USER" | grep -qw "dialout"; then
-                        printf "${YELLOW}User is already in dialout group (ref: /dev/ttyUSBx Error opening serial port)...\n${NC}"
+                    printf "${YELLOW}uucp Group Exists add current user...\n${NC}"
+                    if id -nG "$CURRENT_USER" | grep -qw "uucp"; then
+                        printf "${YELLOW}User is already in uucp group (ref: /dev/ttyUSBx Error opening serial port)...\n${NC}"
                     else
-                        printf "${YELLOW}Add user to dialout group (ref: /dev/ttyUSBx Error opening serial port)...\n${NC}"
-                        sudo usermod -a -G dialout $CURRENT_USER
+                        printf "${YELLOW}Add user to uucp group (ref: /dev/ttyUSBx Error opening serial port)...\n${NC}"
+                        sudo usermod -a -G uucp $CURRENT_USER
                     fi
                 else
-                    printf "${RED}Dialout Group Not Exists can't add current user...\n${NC}"
+                    printf "${RED}uucp Group Not Exists can't add current user...\n${NC}"
                 fi
                 ;;
             xed_res)
